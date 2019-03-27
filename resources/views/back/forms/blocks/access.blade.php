cc2f940734b0642fd81d6b24ac0995ab835d4f5c
Konstantin Bukin <bukin@inetstudio.ru>
2019-03-27 03:13:46 +0300

    Update admin panel to bootstrap 4

--- resources/views/back/forms/blocks/access.blade.php
+++ resources/views/back/forms/blocks/access.blade.php
@@ -6,33 +6,31 @@
 @endphp
 
 @if ($accessConfig)
-    <div class="panel-group float-e-margins" id="accessAccordion">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h5 class="panel-title">
-                    <a data-toggle="collapse" data-parent="#accessAccordion" href="#collapseAccess" aria-expanded="false" class="collapsed">Доступ</a>
-                </h5>
-            </div>
-            <div id="collapseAccess" class="panel-collapse collapse" aria-expanded="false">
-                <div class="panel-body">
-                    @foreach ($accessConfig as $field => $title)
-                        {!! Form::dropdown('access['.$field.'][roles][]', $item->getFieldAccessByKey($field, 'roles'), [
-                            'label' => [
-                                'title' => $title,
-                            ],
-                            'field' => [
-                                'class' => 'select2 form-control',
-                                'data-placeholder' => 'Выберите роли',
-                                'style' => 'width: 100%',
-                                'multiple' => 'multiple',
-                                'data-source' => route('back.acl.roles.getSuggestions'),
-                            ],
-                            'options' => [
-                                'values' => (old('access.'.$field.'.roles')) ? $rolesService->getRolesByIDs(old('access.'.$field.'.roles'), true)->pluck('display_name', 'id')->toArray() : $rolesService->getRolesByIDs($item->getFieldAccessByKey($field, 'roles'), true)->pluck('display_name', 'id')->toArray(),
-                            ],
-                        ]) !!}
-                    @endforeach
-                </div>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h5 class="panel-title">
+                <a data-toggle="collapse" data-parent="#mainAccordion" href="#collapseAccess" aria-expanded="false" class="collapsed">Доступ</a>
+            </h5>
+        </div>
+        <div id="collapseAccess" class="collapse" aria-expanded="false">
+            <div class="panel-body">
+                @foreach ($accessConfig as $field => $title)
+                    {!! Form::dropdown('access['.$field.'][roles][]', $item->getFieldAccessByKey($field, 'roles'), [
+                        'label' => [
+                            'title' => $title,
+                        ],
+                        'field' => [
+                            'class' => 'select2 form-control',
+                            'data-placeholder' => 'Выберите роли',
+                            'style' => 'width: 100%',
+                            'multiple' => 'multiple',
+                            'data-source' => route('back.acl.roles.getSuggestions'),
+                        ],
+                        'options' => [
+                            'values' => (old('access.'.$field.'.roles')) ? $rolesService->getRolesByIDs(old('access.'.$field.'.roles'), true)->pluck('display_name', 'id')->toArray() : $rolesService->getRolesByIDs($item->getFieldAccessByKey($field, 'roles'), true)->pluck('display_name', 'id')->toArray(),
+                        ],
+                    ]) !!}
+                @endforeach
             </div>
         </div>
     </div>
